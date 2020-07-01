48443cd7baf5d0f33416aead9097bf61194634ff
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-01 13:35:31 +0100

    Added functionality for the services.

---
+++ client/src/components/BookingForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <form class="booking-form">
+          <label for="name-field">Enter name</label>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
