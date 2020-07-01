0c8be13894fc723a0404d263b0a23bf98a626d55
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-01 11:52:24 +0100

    Added the get router and the create router file.

--- server/server.js
+++ server/server.js
@@ -16,3 +16,7 @@ MongoClient.connect('mongodb://localhost:27017')
     app.use('/api/bookings', bookingsRouter);
 })
 .catch(console.error)
+
+app.listen(3000, function () {
+    console.log(`Listening on port ${ this.address().port }`);
+});
