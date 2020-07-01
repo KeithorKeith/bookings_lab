781b8726b61a229a98aedfca5e11322d54d1d43c
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-01 13:52:28 +0100

    Added functionality to link front end to back end.

--- client/src/components/BookingForm.vue
+++ client/src/components/BookingForm.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-      <form class="booking-form">
-          <label for="name-field">Enter name</label>
+      <form class="booking-form" v-on:submit="handleBooking">
+          <label for="name-field">Enter name:</label>
+          <input type="text" id="name-field" v-model="name">
+
+          <label for="email-field">Email:</label>
+          <input type="text" id="email-field" v-model="email">
+
+          <label for="checked-in-field">Checked in:</label>
+          <select name="checked-in" id="checked-in-field" v-model="checkedIn">
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+          </select>
+
+          <input type="submit" id="submit-field" value="Submit details" />
       </form>
   </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            name: "",
+            email: "",
+            checkedIn: null
+        }
+    },
+    method: {
+        handleBooking: function
+    }
 }
 </script>
 
